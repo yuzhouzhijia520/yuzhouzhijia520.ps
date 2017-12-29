2b38eabc05c5eec4955de464cc35f502b7ac6afb
yuzhouzhijia520 <qq0236564043>
2017-12-30 00:14:42 +0800

    markdown

--- src/components/page/Markdown.vue
+++ src/components/page/Markdown.vue
@@ -10,17 +10,15 @@
             Vue-SimpleMDE：Vue.js的Markdown Editor组件。
             访问地址：<a href="https://github.com/F-loat/vue-simplemde" target="_blank">Vue-SimpleMDE</a>
         </div>
-        <markdown-editor v-model="content" :configs="configs" ref="markdownEditor"></markdown-editor>
         <div class="plugins-tips">
-            <p>既然用了markdown语法了，那么就有一个很实际的问题了。要怎么在前台展示数据呢？</p>
-            <br>
-            <p>这个时候就需要解析markdown语法了。可以使用 <a href="https://github.com/miaolz123/vue-markdown" target="_blank">vue-markdown</a>：一个基于vue.js的markdown语法解析插件。（这里不作展开，有需要自行了解）</p>
+           <div class="plugins-content" v-html='mdContent'>
+           </div>
         </div>
     </div>
 </template>
 
 <script>
-    import { markdownEditor } from 'vue-simplemde';
+    import marked from 'marked';
     export default {
         data: function(){
             return {
@@ -32,11 +30,70 @@
                         codeSyntaxHighlighting: true,
                         highlightingTheme: 'atom-one-light'
                     }
-                }
+                },
+                mdContent:`
+# MarkDown文件解析-2017-12-29-Blog #
+
+
+## 本地开发 ##
+
+	// 开启服务器，浏览器访问 http://localhost:8080
+	npm run dev
+
+## 构建生产 ##
+
+	// 执行构建命令，生成的dist文件夹放在服务器下即可访问
+	npm run build
+
+## Tables and alignment
+
+First Header | Second Header
+------------ | -------------
+Content from cell 1 | Content from cell 2
+Content in the first column | Content in the second column
+
+| Left-Aligned  | Center Aligned  | Right Aligned |
+| :------------ |:---------------:| -----:|
+| col 3 is      | some wordy text | $1600 |
+| col 2 is      | centered        |   $12 |`
             }
         },
-        components: {
-            markdownEditor
+        methods:{
+            getMarkdownss(){
+                debugger
+              let _this=this;
+            _this.mdContent=marked(`
+# MarkDown文件解析-2017-12-29-Blog #
+
+
+## 本地开发 ##
+
+	// 开启服务器，浏览器访问 http://localhost:8080
+	npm run dev
+
+## 构建生产 ##
+
+	// 执行构建命令，生成的dist文件夹放在服务器下即可访问
+	npm run build
+
+## Tables and alignment
+
+First Header | Second Header
+------------ | -------------
+Content from cell 1 | Content from cell 2
+Content in the first column | Content in the second column
+
+| Left-Aligned  | Center Aligned  | Right Aligned |
+| :------------ |:---------------:| -----:|
+| col 3 is      | some wordy text | $1600 |
+| col 2 is      | centered        |   $12 |`);
+            }
+        },
+        mounted(){
+            debugger
+            let _this=this;
+            _this.getMarkdownss();
+          
         }
     }
 </script>
